5bc424414f4518c06d864ee9c8d553a9ce5bd07a
Anton <balaevantoine@gmaiil.com>
2023-04-26 01:07:35 +0300

    change all

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import telebot
-import config
-import numpy as np
 import pickle
-import nltk
-import re
-nltk.download('stopwords')
-nltk.download('wordnet')
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 token = '5908448826:AAFyiDCuk3Y3i1R-uoEZa1K6jKbjtXFaPAg'
-bot = telebot.TeleBot(config.Token)
+bot = telebot.TeleBot(token)
 
 
 with open('model_clf.pkl', 'rb') as fid:
     model = pickle.load(fid)
 
+with open('cv.pk', 'rb') as fid:
+    cv = pickle.load(fid)
+
 
 def preprocess(user_input):
-    cv = CountVectorizer()
-    return cv.transform(user_input)
+    return cv.transform([user_input])
 
 
 def convert_result(res):
     d = {0: 'Это обычное сообщение. Оно не является ни спамом, ни новостью',
          1: 'Данное сообщение является новостным',
          2: 'Данное сообщение является спамом'}
-    return d[res]
+    return d[res[0]]
 
 
 @bot.message_handler(content_types=["text"])
