fb8d2bd3f2ce1b5a688aea666f2cef3861ebf392
simpleton <pangr33n@gmail.com>
2023-04-27 04:04:14 +0300

    adaboost

--- main.py
+++ main.py
@@ -8,15 +8,29 @@ bot = telebot.TeleBot(token)
 
 
 with open('model_clf.pkl', 'rb') as fid:
-    model = pickle.load(fid)
+    model_multNB = pickle.load(fid)
+
+with open('clf.pkl', 'rb') as fid:
+    model_Adaboost = pickle.load(fid)
 
 with open('cv.pk', 'rb') as fid:
-    cv = pickle.load(fid)
+    multiNB_cv = pickle.load(fid)
+
+with open('cv_adaboost.pk', 'rb') as fid:
+    adaboost_cv = pickle.load(fid)
 
 
-def preprocess(user_input):
-    return cv.transform([user_input])
+def preprocess_multiNB(user_input):
+    return multiNB_cv.transform([user_input])
 
+def preprocess_adaboost(user_input):
+    return adaboost_cv.transform([user_input])
+
+preporocess = [preprocess_multiNB, preprocess_adaboost]
+models = [(model_multNB, 'MultiNB'), (model_Adaboost, 'Adaboost')]
+admins = [274400365, 465949389]
+
+model = 0
 
 def convert_result(res):
     d = {0: 'Это обычное сообщение. Оно не является ни спамом, ни новостью',
@@ -25,12 +39,22 @@ def convert_result(res):
     return d[res[0]]
 
 d = {}
+
 help_msg="""Привет. Я чат-бот, призванный бороться со спамом!
 Пока что я умею отличать только три `типа разных сообщений:
 1) **Спам-сообщения**, за такое ты можешь быть исключен из группы
 2) **Новостные сообщения**, такие сообщения приветсвуются, за них я всегда благодарю
 3) **Обычные сообщения**, я никак не отреагирую на него в групповом чате, но точно скажу тебе в личке, что это не новость и не спам:)"""
 
+@bot.message_handler(commands=["set_model"])
+def set_model(message):
+    if (message.chat.type == 'private' and message.chat.id in admins):
+        global model
+        split = message.text.split()
+        model = int(split[1]) - 1
+        bot.send_message(chat_id=message.chat.id, text=f'Модель изменена на {models[model][1]}')
+    else:
+         bot.send_message(chat_id=message.chat.id, text='Команда доступна только в личных сообщениях и только администратору')
 
 @bot.message_handler(content_types=["new_chat_members"])
 def new_user(message):
@@ -45,9 +69,9 @@ def help(message):
 @bot.message_handler(content_types=["text"])
 def get_messages(message): 
     user_input = message.text
-    preprocessed_input = preprocess(user_input)
-    prediction = model.predict(preprocessed_input)
     if (message.chat.type == 'group' or message.chat.type == 'supergroup'):
+        preprocessed_input = preporocess[model](user_input)
+        prediction = models[model][0].predict(preprocessed_input)
         if(prediction == 2):
             if(message.from_user.id not in d.keys()):
                 d[message.from_user.id]=1
@@ -64,8 +88,12 @@ def get_messages(message):
         elif(prediction == 1):
             bot.send_message(chat_id=message.chat.id, text=f'@{message.from_user.username}, спасибо за актуальные новости!')
     else:
-        convert_res = convert_result(prediction)
-        bot.send_message(message.chat.id, convert_res)
+        res = ''
+        for i in range(len(models)):
+            prep = preporocess[i](user_input)
+            prediction_multiNB = models[i][0].predict(prep)
+            res += f'{models[i][1]}: {convert_result(prediction_multiNB)}\n\n'
+        bot.send_message(message.chat.id, res)
 
 if __name__ == '__main__':
      bot.infinity_polling()
